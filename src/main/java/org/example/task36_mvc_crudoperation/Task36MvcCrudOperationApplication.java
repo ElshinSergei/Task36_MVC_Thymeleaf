028122e68c82fed3d6985e3262b12a478780c8c3
Elshin Sergei <elshin.sergei@list.ru>
2026-02-01 09:10:34 +0500

    first commit

--- src/main/java/org/example/task36_mvc_crudoperation/Task36MvcCrudOperationApplication.java
+++ src/main/java/org/example/task36_mvc_crudoperation/Task36MvcCrudOperationApplication.java
@@ -2,11 +2,20 @@ package org.example.task36_mvc_crudoperation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Task36MvcCrudOperationApplication {
+public class Task36MvcCrudOperationApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+        return builder.sources(Task36MvcCrudOperationApplication.class);
+    }
 
     public static void main(String[] args) {
+
         SpringApplication.run(Task36MvcCrudOperationApplication.class, args);
     }
 
